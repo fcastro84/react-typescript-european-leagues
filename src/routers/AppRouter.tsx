779c4ff1a375b6a2229180939b0df1e93e64dd4f
Fidel <fenrique.castro@gmail.com>
2024-02-02 23:20:54 -0500

    change hashRouter

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Navigate, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 
 import { TeamDetails } from "../components/teams/TeamDetails"
 import CountriesLeagues from "../components/CountriesLeagues"
@@ -6,13 +6,13 @@ import CountriesLeagues from "../components/CountriesLeagues"
 
 const AppRouter = () => {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/league">
         <Routes>
             <Route path="/" element={<Navigate to="/countries-league" replace />} ></Route>
             <Route path="/countries-league" element={<CountriesLeagues />}></Route>
             <Route path="/team/:idLeague/:idTeam" element={<TeamDetails />}></Route>
         </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
